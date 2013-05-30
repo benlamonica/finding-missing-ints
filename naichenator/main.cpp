086c928f16d97f33e9184fd80828ad3fb20f2cc3
Ben La Monica <ben.lamonica@gmail.com>
2013-05-30 13:00:51 -0500

    Added command line paramters to generate the file

--- naichenator/main.cpp
+++ naichenator/main.cpp
@@ -7,12 +7,57 @@
 //
 
 #include <iostream>
+#include <unistd.h>
 #include "RandomFileGenerator.h"
 
-int main(int argc, const char * argv[])
+using std::cout;
+
+void usage(const std::string &errorMessage) {
+    cout << errorMessage << std::endl;
+    cout << "Usage: naichenator [-g -m <minNum> -x <maxNum> -p <probability>] <-f filename>" << std::endl << std::endl;
+}
+
+int main(int argc, char * const argv[])
 {
-    RandomFileGenerator r(0,1000000,0.1);
-    r.write("test");
+    bool generateFile = false;
+    std::string filename;
+    double probability = 1.0;
+    uint32_t min = 0, max = 10;
+    int flag;
+    
+    while ((flag = getopt(argc, argv, "m:x:p:gf:")) != -1) {
+        switch (flag) {
+            case 'm':
+                min = atoi(optarg);
+                break;
+            case 'x':
+                max = atoi(optarg);
+                break;
+            case 'p':
+                probability = atof(optarg);
+                break;
+            case 'g':
+                generateFile = true;
+                break;
+            case 'f':
+                filename = std::string(optarg);
+                break;
+            case '?':
+            default:
+                usage("");
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (filename == "") {
+        usage("You must specify a filename.");
+    }
+    
+    if (generateFile) {
+        RandomFileGenerator r(min,max,probability);
+        r.write(filename);
+    }
     return 0;
 }
 
