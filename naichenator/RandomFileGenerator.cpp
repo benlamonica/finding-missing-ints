51ab230dadf9a92d590c5ab258e6ea9cde9f0fd7
Ben La Monica <ben.lamonica@gmail.com>
2013-05-30 07:56:53 -0500

    Ability to generate random number files

---
+++ naichenator/RandomFileGenerator.cpp
@@ -0,0 +1,9 @@
+//
+//  RandomFileGenerator.cpp
+//  naichenator
+//
+//  Created by Ben La Monica on 5/29/13.
+//  Copyright (c) 2013 Ben La Monica. All rights reserved.
+//
+
+#include "RandomFileGenerator.h"
---
+++ naichenator/RandomFileGenerator.h
@@ -0,0 +1,14 @@
+//
+//  RandomFileGenerator.h
+//  naichenator
+//
+//  Created by Ben La Monica on 5/29/13.
+//  Copyright (c) 2013 Ben La Monica. All rights reserved.
+//
+
+#ifndef __naichenator__RandomFileGenerator__
+#define __naichenator__RandomFileGenerator__
+
+#include <iostream>
+
+#endif /* defined(__naichenator__RandomFileGenerator__) */
